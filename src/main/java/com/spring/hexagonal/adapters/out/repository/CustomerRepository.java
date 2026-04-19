09a0b0672df009cf2fd4bbc5fce03ca63b46dabb
JonasMoura-J <jonasmourat9090@gmail.com>
2026-04-19 16:53:41 -0300

    criando repository no adpater para persistencia no MongoDB

---
+++ src/main/java/com/spring/hexagonal/adapters/out/repository/CustomerRepository.java
@@ -0,0 +1,7 @@
+package com.spring.hexagonal.adapters.out.repository;
+
+import com.spring.hexagonal.adapters.out.repository.entity.CustomerEntity;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public class CustomerRepository extends MongoRepository<CustomerEntity, String> {
+}
