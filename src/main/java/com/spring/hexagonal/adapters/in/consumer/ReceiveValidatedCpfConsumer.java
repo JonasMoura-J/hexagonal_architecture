60de4bf8c6efbbf1340e8071fb1c92f84957a36a
JonasMoura-J <jonasmourat9090@gmail.com>
2026-04-20 16:32:26 -0300

    criando Consumer

---
+++ src/main/java/com/spring/hexagonal/adapters/in/consumer/ReceiveValidatedCpfConsumer.java
@@ -0,0 +1,26 @@
+package com.spring.hexagonal.adapters.in.consumer;
+
+import com.spring.hexagonal.adapters.in.consumer.mapper.CustomerMessageMapper;
+import com.spring.hexagonal.adapters.in.consumer.message.CustomerMessage;
+import com.spring.hexagonal.application.core.domain.Customer;
+import com.spring.hexagonal.application.ports.in.UpdateCustomerInputPort;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ReceiveValidatedCpfConsumer {
+
+    @Autowired
+    private UpdateCustomerInputPort updateCustomerInputPort;
+
+    @Autowired
+    private CustomerMessageMapper customerMessageMapper;
+
+    @KafkaListener(topics = "tp-cpf-validated", groupId = "spring")
+    public void receive(CustomerMessage customerMessage){
+
+        Customer customer = customerMessageMapper.toCustomer(customerMessage);
+        updateCustomerInputPort.update(customer, customerMessage.getZipCode());
+    }
+}
